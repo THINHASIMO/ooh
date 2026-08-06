7d8cc32edb3d59a8a0e1ede49eedc1e89766c963
Thinh Pham <thinhphamndtdtu@gmail.com>
2026-08-06 17:17:50 +0700

    init

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'dich-vu/:slug',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'tin-tuc/:slug',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
